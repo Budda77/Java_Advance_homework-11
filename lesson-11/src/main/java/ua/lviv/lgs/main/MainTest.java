78b3dd3cc38ccc2402ba19541edb43fc9ba3812e
Budda77 <95382143+Budda77@users.noreply.github.com>
2023-01-29 23:13:32 +0200

    Add files via upload

---
+++ lesson-11/src/main/java/ua/lviv/lgs/main/MainTest.java
@@ -0,0 +1,23 @@
+package ua.lviv.lgs.main;
+
+import ua.lviv.lgs.domain.Product;
+import ua.lviv.lgs.domain.User;
+import ua.lviv.lgs.service.ProductService;
+import ua.lviv.lgs.service.UserService;
+import ua.lviv.lgs.service.impl.ProductServiceImpl;
+import ua.lviv.lgs.service.impl.UserServiceImpl;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+//		UserService userService = new UserServiceImpl();
+//		userService.create(new User("test2@com", "secondt Test", "last Test", "USER", "1234"));
+//		
+//		ProductService productService = new ProductServiceImpl();		
+//		productService.create(new Product("Times", "finance", 25.25));
+
+
+	}
+
+}
